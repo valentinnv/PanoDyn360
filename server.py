0b2b268f0741d2251f15901a33ff42e2b508dbf0
valentinv@gmail.com <valentinv@gmail.com>
2025-09-27 12:52:42 +0300

    portchange

--- server.py
+++ server.py
@@ -86,5 +86,5 @@ def run_server(port=8000):
 
 if __name__ == "__main__":
     import sys
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
     run_server(port)
